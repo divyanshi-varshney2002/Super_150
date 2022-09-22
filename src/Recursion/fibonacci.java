94d5d2416e5a291a86c6719fa9b70d43d7455bff
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-09-22 13:40:33 +0530

    NEW

---
+++ src/Recursion/fibonacci.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(fib(n));
+    }
+    public static int fib(int n){
+        if(n==0||n==1) return n;
+        return fib(n-1)+fib(n-2);
+    }
+}
