eafe21c8a324c13fa83e344b6cf7ece92e8c073f
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-09-22 15:45:55 +0530

    NEW

--- src/Recursion/Coin_SampleSpace.java
+++ src/Recursion/Coin_SampleSpace.java
@@ -11,12 +11,14 @@ public class Coin_SampleSpace {
     }
     public static void coin(int n,String ans){
         if(n==0){
-//            if(!ans.contains("HH"))
+//            if(!ans.contains("HH")) //ans nikal k print krte time ki editing
             System.out.println(ans);
             return ;
         }
-        if(ans.length()==0||ans.charAt(ans.length()-1)!='H')
-            coin(n-1,ans+"H");
+//        if(ans.length()==0||ans.charAt(ans.length()-1)!='H')
+//        recursive call ki nhi krna to save time and stack memory
+//        to print the sample space having no two consecutive head
+        coin(n-1,ans+"H");
         coin(n-1,ans+"T");
     }
 }
