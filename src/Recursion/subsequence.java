bc0add28c51af07f7c1c0341c693bffda387b805
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-09-28 13:21:14 +0530

    NEW

--- src/Recursion/subsequence.java
+++ src/Recursion/subsequence.java
@@ -8,6 +8,8 @@ public class subsequence {
         System.out.println("Enter the string:");
         String str= sc.next();
         per(str,"");
+        System.out.println((int)Math.pow(2,str.length()));
+        System.out.println(countper(str,""));
     }
     public static void per(String ques,String ans){
         if(ques.length()==0){
@@ -17,5 +19,15 @@ public class subsequence {
         char ch=ques.charAt(0);
         per(ques.substring(1),ans);
         per(ques.substring(1),ans+ch);
+    }
+    public static int countper(String ques,String ans){
+        if(ques.length()==0){
+//            System.out.println(ans);
+            return 1;
+        }
+        char ch=ques.charAt(0);
+        int ans1 =countper(ques.substring(1),ans);
+        int ans2=countper(ques.substring(1),ans+ch);
+        return ans1+ans2;
     }
 }
