b2823ab8f7d5fd9c6ec0d20d6c52669e0bb9c798
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-09-28 14:31:06 +0530

    NEW

---
+++ src/Recursion/Permutation.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class Permutation {
+    public static void main(String[] args) {
+        String str="abc";
+        permutation(str,"");
+    }
+    public static void permutation(String ques,String ans){
+        if(ques.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        for (int i = 0; i < ques.length(); i++) {
+            char ch=ques.charAt(i);
+            String s1=ques.substring(0,i);
+            String s2=ques.substring(i+1);
+            permutation(s1+s2,ans+ch);
+        }
+    }
+}
