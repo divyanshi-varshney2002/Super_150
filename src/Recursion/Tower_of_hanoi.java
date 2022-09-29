c2435d3855ce4016c7a64249b538bf8e72e9f403
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-09-29 15:39:20 +0530

    NEW

--- src/Recursion/Tower_of_hanoi.java
+++ src/Recursion/Tower_of_hanoi.java
@@ -1,4 +1,17 @@
 package Recursion;
 
 public class Tower_of_hanoi {
+    public static void main(String[] args) {
+        int n=3;
+        String src="A";
+        String hlp="B";
+        String des="C";
+        toh(n,src,hlp,des);
+    }
+    public static void toh(int n,String src,String hlp,String des){
+        if(n==0) return;
+        toh(n-1,src,des,hlp);
+        System.out.println("Move "+n+"th disc from "+src+" to "+des);
+        toh(n-1,hlp,src,des);
+    }
 }
