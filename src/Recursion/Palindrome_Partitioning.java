37843cbabd06fcf0ac52c916f1ab95a074448369
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-10-13 15:09:20 +0530

    NEW

--- src/Recursion/Palindrome_Partitioning.java
+++ src/Recursion/Palindrome_Partitioning.java
@@ -1,18 +1,40 @@
 package Recursion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Palindrome_Partitioning {
     public static void main(String[] args) {
         String str="nitin";
-        Partitioning(str,"");
+        List<String > list= new ArrayList<>();
+        List<List<String>> ans=new ArrayList<>();
+        Partitioning(str,list,ans);
+        System.out.println(ans);
     }
-    public static void Partitioning(String ques,String ans){
+    public static void Partitioning(String ques, List<String> list,List<List<String>> ans){
         if(ques.length()==0){
-            System.out.println(ans);
+//            System.out.println(list);
+            ans.add(new ArrayList<>(list));
             return;
         }
         for (int cut = 1; cut <=ques.length() ; cut++) {
             String s=ques.substring(0,cut);
-            Partitioning(ques.substring(cut),ans+s+"|");
+            if (isPalindrome(s)) {
+                list.add(s);
+                Partitioning(ques.substring(cut), list,ans);
+                list.remove(list.size()-1);
+            }
         }
+    }
+    public static boolean isPalindrome(String str){
+        int i=0;
+        int j=str.length()-1;
+        while (i<j){
+            if(str.charAt(i)!=str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
     }
 }
