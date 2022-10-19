8fd218efe1e6eb0bc98eba890bd0a6422df1144e
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-10-19 11:39:37 +0530

    NEW

---
+++ src/Recursion/Combination_sumII.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+import java.util.Arrays;
+
+public class Combination_sumII {
+    public static void main(String[] args) {
+        int arr[]={10,1,2,7,6,1,5};
+        int target=8;
+        Arrays.sort(arr);
+        Combination(arr,target,"",0);
+    }
+    public static void Combination(int arr[],int target,String ans,int idx){
+        if(target==0){
+            System.out.println(ans);
+            return;
+        }
+        for (int i=idx;i<arr.length;i++){
+            if(target>=arr[i]){
+                Combination(arr,target-arr[i],ans+arr[i]+" ",i+1);
+            }
+        }
+    }
+}
