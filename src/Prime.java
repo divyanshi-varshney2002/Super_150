1429aad418269b5f99c358b64c15bcead1c4962c
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-11-03 14:28:16 +0530

    NEW

---
+++ src/Prime.java
@@ -0,0 +1,15 @@
+public class Prime {
+    public static void main(String[] args) {
+        System.out.println(isPrime(31));
+    }
+    public static boolean isPrime(int n){
+        int div=2;
+        while (div*div<=n){
+            if(n%div==0){
+                return false;
+            }
+            div++;
+        }
+        return true;
+    }
+}
---
+++ src/Time_and_Space_Complexity/Time_Space_Demo.java
@@ -0,0 +1,19 @@
+package Time_and_Space_Complexity;
+
+public class Time_Space_Demo {
+    public static void main(String[] args) {
+        long start=System.currentTimeMillis();
+        for (int i = 0; i < 1000000; i++) {
+
+        }
+        long end=System.currentTimeMillis();
+        System.out.println(end-start);
+    }
+}
+/*
+Asymtotic notation
+        1.best  omega
+        2.avg   theeta
+        3.worst case    BigO
+
+ */
