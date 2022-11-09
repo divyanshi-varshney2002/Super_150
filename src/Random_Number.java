fca3f06362d0c96dc61d92b4a0f17c208ba6b3c6
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-11-09 15:04:39 +0530

    NEW

---
+++ src/Random_Number.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Random_Number {
+    public static void main(String[] args) {
+        int lo=10;
+        int hi=100;
+        Random r=new Random();
+        for (int i = 0; i < 40; i++) {
+            int rn=r.nextInt(hi-lo+1)+lo;
+            System.out.print(rn+" ");
+        }
+    }
+}
