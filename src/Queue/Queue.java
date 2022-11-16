996cec256a340deeb0d6813ed6f7ab0c57dbf3b3
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-11-16 15:31:27 +0530

    NEW

---
+++ src/Queue/Queue.java
@@ -0,0 +1,42 @@
+package Queue;
+
+public class Queue {
+    /*
+    FIFO (First in first Out)
+    LILO (Last In Last Out)
+     */
+    private int arr[];
+    private int front;
+    private int size;
+    public Queue(){
+        arr=new int [5];
+        front=0;
+        size=0;
+    }
+    public Queue(int n){
+        arr=new int [n];
+        front=0;
+        size=0;
+    }
+    public boolean isEmpty(){
+        return size==0;
+    }
+    public boolean isFull(){
+        return size==arr.length;
+    }
+    public void Enqueue(int item){
+        isFull() ;
+        arr[(front+size)%arr.length]=item;
+        size++;
+    }
+    public int Dequeue(){
+        int rv=arr[front];
+        front++;
+        size--;
+        return rv;
+    }
+    public int GetFront() {
+        int rv=arr[front];
+        return rv;
+    }
+}
