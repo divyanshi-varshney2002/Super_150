81bf4224cbdcfbc2ed202f07fc4a70d513feb9ba
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-11-17 14:49:36 +0530

    NEW

--- src/Inheritance/Client.java
+++ src/Inheritance/Client.java
@@ -13,12 +13,35 @@ public class Client {
 //        obj.fun1();
 
         //case2
-        P obj=new C();
-        System.out.println(obj.d);//1
-        System.out.println(obj.d1);//10
-        System.out.println(((C)(obj)).d2);//20  -type cast the object to access the members of the class
-        System.out.println(((C)(obj)).d);//2
-        obj.fun();
-        obj.fun1();
+//        P obj=new C();
+//        System.out.println(obj.d);//1
+//        System.out.println(obj.d1);//10
+//        System.out.println(((C)(obj)).d2);//20  -type cast the object to access the members of the class
+//        System.out.println(((C)(obj)).d);//2
+//        obj.fun();
+//        obj.fun1();
+//        ((C)(obj)).fun2();
+
+        //case3
+//        C obj=new P();
+//        System.out.println(obj.d);
+//        System.out.println(obj.d1);
+//        System.out.println(obj.d2);
+        //It is not allowed in inheritance
+
+        //case 4
+//        C obj=new C();
+//        System.out.println(obj.d);
+//        System.out.println(obj.d1);
+//        System.out.println(obj.d2);
+//        System.out.println(((P)(obj)).d);
+//        obj.fun();
+//        obj.fun1();
+//        obj.fun2();
+
+        //2different class
+        //inheritance
+        //the method with same name should be in  both class;
+        //this is called method overriding
     }
 }
