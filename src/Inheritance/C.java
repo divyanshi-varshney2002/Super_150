064fdea41aeb28700045192d60e25915c63c3ae8
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-11-17 13:39:16 +0530

    NEW

---
+++ src/Inheritance/C.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class C {
+    int d=2;
+    int d2=20;
+    public void fun(){
+        System.out.println("fun in C");
+    }
+    public void fun1(){
+        System.out.println("fun1 in C");
+    }
+}
---
+++ src/Inheritance/Client.java
@@ -0,0 +1,7 @@
+package Inheritance;
+
+public class Client {
+    public static void main(String[] args) {
+        P obj=new P();
+    }
+}
