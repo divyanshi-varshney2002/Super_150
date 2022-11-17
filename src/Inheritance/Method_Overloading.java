e0c0e33b1d32899c03a17843024e04dd5d557bd5
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-11-17 14:59:47 +0530

    NEW

---
+++ src/Inheritance/Method_Overloading.java
@@ -0,0 +1,30 @@
+package Inheritance;
+
+public class Method_Overloading {
+    public static void main(String[] args) {
+        //overloading
+        //same class
+        //method with same name and return type
+        //method with different types of argument or no. of argument
+        System.out.println(add(2,3));
+        System.out.println(add(2,3,4));
+        System.out.println(add(2,3,4.8f));
+        System.out.println(add(1,3,6,89,4,7,-4,6,-7,45,3));
+    }
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static int add(int a,int b,int c){
+        return a+b+c;
+    }
+    public static int add(int a,int b,float c){
+        return (int)(a+b+c);
+    }
+    public static int add(int ... a){
+        int sum=0;
+        for(int i=0;i<a.length;i++){
+            sum+=a[i];
+        }
+        return sum;
+    }
+}
