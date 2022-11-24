bbb5455b2ed9d4229a0d201ef7a47058f75b1793
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-11-24 15:32:29 +0530

    NEW

---
+++ src/Stack/Insert_Down.java
@@ -0,0 +1,25 @@
+package Stack;
+
+import java.util.Stack;
+public class Insert_Down {
+    public static void main(String[] args) {
+        Stack<Integer> st=new Stack<>();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.push(50);
+        System.out.println(st);
+        Insert(st,-1);
+        System.out.println(st);
+    }
+    public static void Insert(Stack<Integer> st,int item){
+        if(st.isEmpty()){
+            st.push(item);
+            return;
+        }
+        int x=st.pop();
+        Insert(st,item);
+        st.push(x);
+    }
+}
