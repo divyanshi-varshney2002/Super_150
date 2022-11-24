70784a456f53d8c6f091a1a7f451483ca6d907a9
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-11-24 15:42:37 +0530

    NEW

---
+++ src/Stack/Reverse_Stack.java
@@ -0,0 +1,34 @@
+package Stack;
+
+import java.util.Stack;
+
+public class Reverse_Stack {
+    public static void main(String[] args) {
+        Stack<Integer> st=new Stack<>();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.push(50);
+        System.out.println(st);
+        reverse(st);
+        System.out.println(st);
+    }
+    public static void reverse(Stack<Integer> st){
+        if (st.isEmpty()){
+            return;
+        }
+        int x=st.pop();
+        reverse(st);
+        Insert(st,x);
+    }
+    public static void Insert(Stack<Integer> st,int item){
+        if(st.isEmpty()){
+            st.push(item);
+            return;
+        }
+        int x=st.pop();
+        Insert(st,item);
+        st.push(x);
+    }
+}
