23486db89847bf01436d97db898b89c10ec2d63e
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-11-24 14:16:37 +0530

    NEW

---
+++ src/Stack/Next_Greater_Element.java
@@ -0,0 +1,25 @@
+package Stack;
+
+import java.util.Stack;
+public class Next_Greater_Element {
+    public static void main(String[] args) {
+        int arr[]={10,2,1,3,5,4,8,7};
+        NGE(arr);
+    }
+    public static void NGE(int arr[]){
+        int ans[]=new int[arr.length];
+        Stack<Integer> st=new Stack<>();
+        for (int i = 0; i <arr.length ; i++) {
+           while (!st.isEmpty() && arr[i]>arr[st.peek()]){
+               ans[st.pop()]=arr[i];
+           }
+           st.push(i);
+        }
+        while (!st.isEmpty()){
+            ans[st.pop()]=-1;
+        }
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(arr[i]+" "+ans[i]);
+        }
+    }
+}
