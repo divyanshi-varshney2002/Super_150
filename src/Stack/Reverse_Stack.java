b1c8df8bf494615ccdeaaba413542008e6c00e96
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-11-24 15:43:13 +0530

    NEW

--- src/Stack/Reverse_Stack.java
+++ src/Stack/Reverse_Stack.java
@@ -1,10 +1,9 @@
 package Stack;
 
 import java.util.Stack;
-
 public class Reverse_Stack {
     public static void main(String[] args) {
-        Stack<Integer> st=new Stack<>();
+        Stack<Integer> st= new Stack<>();
         st.push(10);
         st.push(20);
         st.push(30);
