94ccae5ff5668bb8c9ba48a741c85b5d0e794a8f
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-12-01 13:53:44 +0530

    NEW

---
+++ src/Queue/Number_of_Valid_Subarrays.java
@@ -0,0 +1,15 @@
+package Queue;
+
+/*
+Given an array A of integers, return the number of non-empty continuous subarrays that satisfy the following condition:
+The leftmost element of the subarray is not larger than other elements in the subarray.
+Example 1:
+        Input: [1,4,2,5,3]
+        Output: 11
+        Explanation: There are 11 valid subarrays: [1],[4],[2],[5],[3],[1,4],[2,5],[1,4,2],[2,5,3],[1,4,2,5],[1,4,2,5,3].
+*/
+public class Number_of_Valid_Subarrays {
+    public static void main(String[] args) {
+
+    }
+}
