c9f5d506de6e0afb969924eba6af813f6c7e135b
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-12-01 14:08:10 +0530

    NEW

--- src/Queue/Number_of_Valid_Subarrays.java
+++ src/Queue/Number_of_Valid_Subarrays.java
@@ -1,5 +1,7 @@
 package Queue;
 
+import java.util.Stack;
+
 /*
 Given an array A of integers, return the number of non-empty continuous subarrays that satisfy the following condition:
 The leftmost element of the subarray is not larger than other elements in the subarray.
@@ -10,6 +12,19 @@ Example 1:
 */
 public class Number_of_Valid_Subarrays {
     public static void main(String[] args) {
-
+        int arr[]={1,4,2,5,3};
+        System.out.println(ValidSubarray(arr));
+    }
+    public static int ValidSubarray(int arr[]){
+        Stack<Integer> st=new Stack<>();
+        int ans=0;
+        for (int i=0;i<arr.length;i++){
+            while(!st.isEmpty() && arr[i]<st.peek()){
+                st.pop();
+            }
+            st.push(arr[i]);
+            ans+=st.size();
+        }
+        return ans;
     }
 }
