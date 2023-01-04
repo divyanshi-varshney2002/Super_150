5e165ff18a23769548dc580028eef181e4ed8f6c
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2023-01-04 15:11:09 +0530

    NEW

--- src/Linked_List/LinkedList.java
+++ src/Linked_List/LinkedList.java
@@ -41,6 +41,22 @@ public class LinkedList {
         }
     }
 
+    public void addAtIndex(int item,int k) throws Exception{
+        if(k<0||k>size){
+            throw new Exception("k out of index");
+        }
+        if(k==0) addFirst(item);
+        else if(k==size) addLast(item);
+        else{
+            Node k_1th=getNode(k-1);
+            Node nn=new Node();
+            nn.val=item;
+            nn.next=k_1th.next;
+            k_1th.next=nn;
+            size++;
+        }
+    }
+
     public void display(){
         Node temp=head;
         while (temp!=null){
@@ -50,14 +66,42 @@ public class LinkedList {
         System.out.println("NULL");
     }
 
-    public static void main(String[] args) {
+    private Node getNode(int k) throws Exception {
+        if(k<0||k>size){
+            throw new Exception("k out of index");
+        }
+        Node temp=head;
+        for(int i=1;i<=k;i++){
+            temp=temp.next;
+        }
+        return temp;
+    }
+
+    public int getFirst(){
+        return head.val;
+    }
+    public int getLast(){
+        return tail.val;
+    }
+
+    public int getAtIndex(int k) throws Exception {
+        return getNode(k).val;
+    }
+
+    public static void main(String[] args) throws Exception {
         Scanner sc=new Scanner(System.in);
         LinkedList ll =new LinkedList();
         ll.addFirst(10);
         ll.addFirst(20);
         ll.addFirst(30);
+        ll.display();
         ll.addLast(40);
         ll.addLast(50);
         ll.display();
+        ll.addAtIndex(0,3);
+        ll.display();
+        System.out.println(ll.getFirst());
+        System.out.println(ll.getLast());
+        System.out.println(ll.getAtIndex(4));
     }
 }
