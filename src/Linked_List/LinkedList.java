536e7d6b533a162546e0cb2ad9adc46b7affc1cd
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2023-01-04 14:43:24 +0530

    NEW

--- src/Linked_List/LinkedList.java
+++ src/Linked_List/LinkedList.java
@@ -1,5 +1,7 @@
 package Linked_List;
 
+import java.util.Scanner;
+
 public class LinkedList {
     private class Node{
         int val;
@@ -22,5 +24,40 @@ public class LinkedList {
             head=nn;
             size++;
         }
+    }
+
+    public void addLast(int item){
+        Node nn=new Node();
+        nn.val=item;
+        if(this.size==0){
+            head=nn;
+            tail=nn;
+            size++;
+        }
+        else{
+            tail.next=nn;
+            tail=nn;
+            size++;
+        }
+    }
+
+    public void display(){
+        Node temp=head;
+        while (temp!=null){
+            System.out.print(temp.val+"-->");
+            temp=temp.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        LinkedList ll =new LinkedList();
+        ll.addFirst(10);
+        ll.addFirst(20);
+        ll.addFirst(30);
+        ll.addLast(40);
+        ll.addLast(50);
+        ll.display();
     }
 }
