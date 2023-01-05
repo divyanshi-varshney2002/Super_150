19a193afcd7c46ac23ba655ecf28cefc90cbc881
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2023-01-05 09:35:55 +0530

    NEW

---
+++ src/Linked_List/Reverse_LinkedList.java
@@ -0,0 +1,29 @@
+package Linked_List;
+
+//public class Reverse_LinkedList {
+//}
+
+
+// * Definition for singly-linked list.
+
+class Reverse_LinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode reverseList(ListNode head) {
+        ListNode curr=head;
+        ListNode prev=null;
+        while(curr!=null){
+            ListNode ahead=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=ahead;
+        }
+        return prev;
+    }
+
+}
