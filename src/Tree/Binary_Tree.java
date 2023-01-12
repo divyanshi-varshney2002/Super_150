dee6b7f01471d53400163896cf06ce9e2e460729
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2023-01-12 13:51:44 +0530

    NEW

--- src/Tree/Binary_Tree.java
+++ src/Tree/Binary_Tree.java
@@ -49,4 +49,89 @@ public class Binary_Tree {
         display(node.left);
         display(node.right);
     }
+    public void PreOrder(){
+        PreOrder(root);
+        System.out.println();
+    }
+    private void PreOrder(Node node){
+        if(node==null) return;
+        System.out.print(node.val+" ");
+        PreOrder(node.left);
+        PreOrder(node.right);
+    }
+    public void PostOrder(){
+        PostOrder(root);
+        System.out.println();
+    }
+    private void PostOrder(Node node){
+        if(node==null) return;
+        PostOrder(node.left);
+        PostOrder(node.right);
+        System.out.print(node.val+" ");
+    }
+    public void InOrder(){
+        InOrder(root);
+        System.out.println();
+    }
+    private void InOrder(Node node){
+        if(node==null) return;
+        InOrder(node.left);
+        System.out.print(node.val+" ");
+        InOrder(node.right);
+    }
+    public boolean search(int item){
+        return search(root,item);
+    }
+    private boolean search(Node node,int item){
+        if(node==null) return false;
+        if(node.val==item) return true;
+        boolean left=search(node.left,item);
+        boolean right=search(node.right,item);
+        return left || right;
+    }
+//    10 true 30 true -7 false false true 50 true 11 false true 12 true 13 false false false true 180 false false true 140 true 170 false false true 180 true 120 false false true 130 true 110 false true 12 false false false
+//            30<--10-->140
+//            -7<--30-->50
+//            .<---7-->.
+//            11<--50-->180
+//            .<--11-->12
+//            13<--12-->.
+//            .<--13-->.
+//            .<--180-->.
+//            170<--140-->180
+//            .<--170-->.
+//            120<--180-->130
+//            .<--120-->.
+//            110<--130-->.
+//            .<--110-->12
+//            .<--12-->.
+    public int Max(){
+        return Max(root);
+    }
+    private int Max(Node node){
+        if(node==null) return Integer.MIN_VALUE ;
+        int left=Max(node.left);
+        int right=Max(node.right);
+        return Math.max(node.val,Math.max(left,right));
+    }
+
+    public int Min(){
+        return Min(root);
+    }
+    private int Min(Node node){
+        if(node==null) return Integer.MAX_VALUE ;
+        int left=Min(node.left);
+        int right=Min(node.right);
+        return Math.min(node.val,Math.min(left,right));
+    }
+
+    public int Height(){
+        return Height(root);
+    }
+    private int Height(Node node){
+        if(node==null) return -1;
+        int left=Height(node.left);
+        int right=Height(node.right);
+        return Math.max(left,right)+1;
+    }
 }
--- src/Tree/Binary_Tree_Client.java
+++ src/Tree/Binary_Tree_Client.java
@@ -4,5 +4,11 @@ public class Binary_Tree_Client {
     public static void main(String[] args) {
         Binary_Tree bt=new Binary_Tree();
         bt.display();
+//        bt.PreOrder();
+//        bt.PostOrder();
+//        bt.InOrder();
+//        System.out.println(bt.search(140));
+        System.out.println(bt.Max());
+        System.out.println(bt.Height());
     }
 }
