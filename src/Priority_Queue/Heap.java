b77a6ae485db517dd6d19b5bbbc1cd5829b2d5d4
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2023-02-16 10:23:05 +0530

    NEW

--- src/Priority_Queue/Heap.java
+++ src/Priority_Queue/Heap.java
@@ -21,4 +21,32 @@ public class Heap {
         list.set(i,jth);
         list.set(j,ith);
     }
+    private int max(){
+        swap(0, list.size()-1);
+        int rv=list.remove(list.size()-1);
+        downheapify(0);
+        return rv;
+    }
+    private void downheapify(int pi){
+        int lci=2*pi+1;
+        int rci=2*pi+2;
+        int mini=pi;
+        if(lci< list.size()&&list.get(mini)>list.get(lci))
+            mini=lci;
+        if(lci< list.size()&&list.get(mini)>list.get(rci))
+            mini=rci;
+        if(mini!=pi) {
+            swap(mini, pi);
+            downheapify(mini);
+        }
+    }
+    private int min(){
+        return list.get(0);
+    }
+    private int size(){
+        return list.size();
+    }
+    public void display(){
+        System.out.println(list);
+    }
 }
